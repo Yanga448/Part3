2fd02a7274400ae94e3e02cfd86168fbcc934610
Yanga448 <130042573+Yanga448@users.noreply.github.com>
2024-06-14 21:07:51 +0000

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace PPart_3
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateRecipe_Click(object sender, RoutedEventArgs e)
+        {
+            var recipeCreatorWindow = new RecipeCreator();
+            recipeCreatorWindow.Show();
+        }
+
+        private void ViewRecipes_Click(object sender, RoutedEventArgs e)
+        {
+            var recipeCollectionWindow = new RecipeCollection();
+            recipeCollectionWindow.Show();
+        }
+
+        private void ViewSpecificRecipe_Click(object sender, RoutedEventArgs e)
+        {
+            var recipeViewerWindow = new RecipeViewer();
+            recipeViewerWindow.Show();
+        }
+    }
+}
+
